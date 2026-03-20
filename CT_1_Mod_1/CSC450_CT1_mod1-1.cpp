7ab3727e028bbb5b4a4d51fbc55ecb46ed720fc7
Bill Keller <william.Keller@csuglobal.edu>
2026-03-19 21:22:13 -0400

    Fixed error filled files

---
+++ CT_1_Mod_1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,24 @@
+/* Simple Program with a few Errors for Correction
+ * Please be sure to correct all outlined errors. */ // << added end of block comment.
+
+
+#include<iostream>
+//#include<conio.h> //I don't think this is needed.
+
+//Standard namespace declaration << Wasn't comment out
+using namespace std;
+
+//Main Function << Wasn't commented out
+int main()
+{
+
+     //Standard Ouput Statement
+     cout << "Welcome to this C++ Program" << endl;
+
+     cout << "I have corrected all errors for this program." << endl;
+
+     // Wait For Output Screen
+
+     //Main Function return Statement
+
+}
---
+++ CT_1_Mod_2/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,27 @@
+
+
+#include<iostream>
+#include<conio.h>  //  I don't think this is needed.
+
+//Standard namespace declaration  << This wasn't commented
+using namespace std;
+
+//Main Function  << This wasn't commented
+int main()
+{
+
+     double myMoney = 1000.50;  //this should be printed out //++ -> What do you mean this should be printed out?  Now?  Later?
+
+
+     //Standard Ouput Statement << This wasn't commented
+     cout << "Please be sure to correct all syntax errors in this program"<< endl; // This needed "<<" between cout and the string
+
+     cout << "I have corrected all errors for this program." << endl; // add end quote at end of string
+
+     cout << "The total amount of money available is = "<< myMoney << endl;  // added money ammount to print here.  I'm guessing this is where it was supposed to print?
+
+     // Wait For Output Screen
+
+     //Main Function return Statement << This wasn't commented
+     return 0;
+}
